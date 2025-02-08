37404082300ec583288104866f186fe888af8667
peterdammy <peterosunsan2@gmail.com>
2025-02-08 22:38:49 +0100

    fifth

---
+++ lib/model/food_model.dart
@@ -0,0 +1,14 @@
+class FoodModel {
+  FoodModel(
+      {required this.id,
+      required this.title,
+      required this.description,
+      required this.price,
+      required this.image});
+
+  final String id;
+  final String title;
+  final String description;
+  final String price;
+  final String image;
+}
---
+++ lib/provider/food_provider.dart
@@ -0,0 +1,28 @@
+import 'package:my_project/model/food_model.dart';
+
+final List<FoodModel> allFoodContent = [
+  FoodModel(
+      id: '1',
+      title: 'Rice and Chicken Stew',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ',
+      price: '3000.00',
+      image: 'assets/image1.jpg'),
+  FoodModel(
+      id: '2',
+      title: 'Egg Roll',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ',
+      price: '1400.00',
+      image: 'assets/image2.jpg'),
+  FoodModel(
+      id: '3',
+      title: 'Spaghetti Bolognese',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ',
+      price: '2800.00',
+      image: 'assets/image3.jpg'),
+  FoodModel(
+      id: '4',
+      title: 'Tradditional Rice',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ',
+      price: '3300.00',
+      image: 'assets/image4.jpg'),
+];
